cf148c824ef22659fb16b3fa03589249e25e2e85
paula garcia <paula.garciadv@gmail.com>
2021-11-10 13:06:32 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+5.times do
+  Restaurant.create!(name:Faker::Coffee.blend_name,
+                    address:Faker::Address.full_address,
+                    phone_number:Faker::PhoneNumber.phone_number,
+                    category:["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
